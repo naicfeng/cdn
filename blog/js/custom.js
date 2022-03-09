0466c608141bb34b382dd89cf6afe75c1798b6c1
GitHub Push Action <push@no-reply.github.com>
2022-03-09 10:50:18 +0000

    GitHub Push

--- blog/js/custom.js
+++ blog/js/custom.js
@@ -16,8 +16,40 @@
       '<div class="h6">文章时效性提示</div><p>这是一篇' + showtext + '于 ' + days + ' 天前的文章，部分信息可能已发生改变，请注意甄别。' +
       '</p></p></div>' + posts[0].innerHTML;
   }
-})();
-(function(){
+	//if (document.domain != 'cuojue.org' && document.domain != 'localhost')window.location.href='https://cuojue.org';
+	if (top.location != self.location)top.location=self.location;
+	var dynamicLoading = {
+		css: function(path){
+				if(!path || path.length === 0){
+				  throw new Error('argument "path" is required !');
+				}
+				var head = document.getElementsByTagName('head')[0];
+				var link = document.createElement('link');
+				link.href = path;
+				link.rel = 'stylesheet';
+				link.type = 'text/css';
+				head.appendChild(link);
+		  },
+		js: function(path){
+			if(!path || path.length === 0){
+			  throw new Error('argument "path" is required !');
+			}
+			var head = document.getElementsByTagName('head')[0];
+			var script = document.createElement('script');
+			script.src = path;
+			script.type = 'text/javascript';
+			head.appendChild(script);
+		  }
+	}
+	if(document.getElementById("hexo-blog-encrypt")){
+		const mainElement = document.getElementById('hexo-blog-encrypt');
+		mainElement.setAttribute('data-wpm','抱歉, 这个密码看着不太对, 请再试试.');
+		mainElement.setAttribute('data-whm','抱歉, 这个文章不能被校验, 不过您还是能看看解密后的内容.');
+		//动态加载 CSS 文件
+		dynamicLoading.css("/css/hbe.style.css");
+		//动态加载 JS 文件
+		dynamicLoading.js("/lib/hbe.js");
+	}
     var bp = document.createElement('script');
     var curProtocol = window.location.protocol.split(':')[0];
     if (curProtocol === 'https') {
@@ -29,38 +61,3 @@
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(bp, s);
 })();
-//if (document.domain != 'cuojue.org' && document.domain != 'localhost')window.location.href='https://cuojue.org';
-if (top.location != self.location)top.location=self.location;
-
-var dynamicLoading = {
-    css: function(path){
-            if(!path || path.length === 0){
-              throw new Error('argument "path" is required !');
-            }
-            var head = document.getElementsByTagName('head')[0];
-            var link = document.createElement('link');
-            link.href = path;
-            link.rel = 'stylesheet';
-            link.type = 'text/css';
-            head.appendChild(link);
-      },
-    js: function(path){
-        if(!path || path.length === 0){
-          throw new Error('argument "path" is required !');
-        }
-        var head = document.getElementsByTagName('head')[0];
-        var script = document.createElement('script');
-        script.src = path;
-        script.type = 'text/javascript';
-        head.appendChild(script);
-      }
-}
-if(document.getElementById("hexo-blog-encrypt")){
-    const mainElement = document.getElementById('hexo-blog-encrypt');
-    mainElement.setAttribute('data-wpm','抱歉, 这个密码看着不太对, 请再试试.');
-    mainElement.setAttribute('data-whm','抱歉, 这个文章不能被校验, 不过您还是能看看解密后的内容.');
-    //动态加载 CSS 文件
-    dynamicLoading.css("/css/hbe.style.css");
-    //动态加载 JS 文件
-    dynamicLoading.js("/lib/hbe.js");
-}
